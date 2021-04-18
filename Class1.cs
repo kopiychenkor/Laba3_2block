8f08b7318d5cd4d0898b15cf18f05c2dcf5009bf
Rostok <79173041+kopiychenkor@users.noreply.github.com>
2021-04-18 11:57:42 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+struct Student
+{
+	public string surName;
+	public string firstName;
+	public string patronymic;
+	public char sex;
+	public string dateOfBirth;
+	public char mathematicsMark;
+	public char physicsMark;
+	public char informaticsMark;
+	public int scholarship;
+
+	public Student(string LineWithAllData)
+	{
+		string[] dataSplitted = LineWithAllData.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+		surName = dataSplitted[0];
+		firstName = dataSplitted[1];
+		patronymic = dataSplitted[2];
+		sex = Convert.ToChar(dataSplitted[3]);
+		dateOfBirth = dataSplitted[4];
+		mathematicsMark = Convert.ToChar(dataSplitted[5]);
+		physicsMark = Convert.ToChar(dataSplitted[6]);
+		informaticsMark = Convert.ToChar(dataSplitted[7]);
+		scholarship = Convert.ToInt32(dataSplitted[8]);
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace struck_lab_student
+{
+    partial class Program
+    {
+        static void СonsolOutput(Student student, int studsNum)
+        {
+            Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8}",
+                student.surName, student.firstName, student.patronymic, student.sex, student.dateOfBirth,
+                student.mathematicsMark, student.physicsMark, student.informaticsMark, student.scholarship, studsNum);
+        }
+        static void Audit(Student student, int studsNum)
+        {
+            string[] dataFromFile = File.ReadAllLines("data.txt");
+            if (student.sex == 'М') 
+            {
+                if ((student.mathematicsMark == '-') && (student.physicsMark == '-'))
+                {
+                    Student[] resStudent = new Student[dataFromFile.Length];
+                    for (int i = 0; i < dataFromFile.Length; i++)
+                    {
+                        resStudent[i] = new Student(dataFromFile[i]);
+                    }
+                    for (int i = 0; i < resStudent.Length; i++)
+                        СonsolOutput(resStudent[i], i + 1);
+                }
+                if((student.physicsMark == '-') && (student.informaticsMark == '-'))
+                {
+                    Student[] resStudent = new Student[dataFromFile.Length];
+                    for (int i = 0; i < dataFromFile.Length; i++)
+                    {
+                        resStudent[i] = new Student(dataFromFile[i]);
+                    }
+                    for (int i = 0; i < resStudent.Length; i++)
+                        СonsolOutput(resStudent[i], i + 1);
+                }
+                if((student.mathematicsMark == '-') && (student.informaticsMark == '-'))
+                {
+                    Student[] resStudent = new Student[dataFromFile.Length];
+                    for (int i = 0; i < dataFromFile.Length; i++)
+                    {
+                        resStudent[i] = new Student(dataFromFile[i]);
+                    }
+                    for (int i = 0; i < resStudent.Length; i++)
+                        СonsolOutput(resStudent[i], i + 1);
+                }
+            }
+        }
+
+        static Student[] ReadData(string fileName)
+        {
+            string[] dataFromFile = File.ReadAllLines("data.txt");
+            Student[] resStud = new Student[dataFromFile.Length];
+            for (int i = 0; i < dataFromFile.Length; i++)
+            {
+                resStud[i] = new Student(dataFromFile[i]);
+                
+            }
+            for (int i = 0; i < resStud.Length; i++)
+                    СonsolOutput(resStud[i], i + 1);
+            return resStud;
+        }
+        static void runMenu(Student[] studs)
+        {
+            
+        }
+        static void Main()
+        {
+            Student[] studs = ReadData("data.txt");
+            runMenu(studs);
+            Console.ReadKey();
+        }
+    }
+}
